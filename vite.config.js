316dd459d650e2b7ede5528da76d59d270ef4c7c
Guillaume Aguiraud <guillaume.aguiraud@gmail.com>
2025-09-04 10:35:23 -0400

    Fix vercel config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, process.cwd(), '')
-  
-  
+
+
   return {
     plugins: [react()],
     server: {
       port: parseInt(env.VITE_PORT) || 5173,
-      proxy: {
+      proxy: env.VITE_API_URL ? {
+        '/api': env.VITE_API_URL,
+        '/ws': {
+          target: env.VITE_API_URL.replace('http', 'ws'),
+          ws: true
+        },
+        '/shell': {
+          target: env.VITE_API_URL.replace('http', 'ws'),
+          ws: true
+        }
+      } : {
         '/api': `http://localhost:${env.PORT || 3001}`,
         '/ws': {
           target: `ws://localhost:${env.PORT || 3001}`,
